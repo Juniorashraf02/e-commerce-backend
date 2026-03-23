daf6ede3df565bddf2738abd37e94254199fd960
juniorashraf02 <shuvo.istiak11@gmail.com>
2026-03-23 14:24:56 +0600

    dotenv setup

--- server/src/app.js
+++ server/src/app.js
@@ -2,15 +2,27 @@ const express = require('express');
 const morgan = require('morgan');
 const router = express.Router();
 const createError = require('http-errors');
+// const xssClean = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+
+
 const app = express();
 
+const rateLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, //1 minute
+    max: 3,
+    message: 'Please try again later',
+});
+
+app.use(rateLimiter);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(xssClean());
 
 app.get('/test', (req, res) => {
     res.status(200).send({
-        message: "everything is working fine"
+        message: "get api is working fine"
     })
 });
 
@@ -34,16 +46,19 @@ app.delete('/test', (req, res) => {
 
 
 // client side error handling
-app.use((req, res, next)=>{
-  next(createError(404, 'route not found'));
+app.use((req, res, next) => {
+    //   next(createError(404, 'route not found'));
+    return res.status(404).json({
+        message: 'Route not found'
+    });
 });
 
 // server side error handling
-app.use((err, req, res, next)=>{
-   return res.status(err.status || 500).json({
-    success: false,
-    message: err.message
-   })
+app.use((err, req, res, next) => {
+    return res.status(err.status || 500).json({
+        success: false,
+        message: err.message || 'internal server error'
+    })
 });
 
 
--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,12 @@
 const app = require("./app");
+require('dotenv').config();
+// console.log(process.env.SERVER_PORT);
 
+const {serverPort} = require("./secret");
+// const serverPort = 3000;
+// const serverPort = process.env.SERVER_PORT || 9500;
 
-app.listen(3000, () => {
-    console.log(`server is running at http://localhost:3000`);
+app.listen(serverPort, () => {
+    console.log(`server is running at http://localhost:${serverPort}`);
 });
+
